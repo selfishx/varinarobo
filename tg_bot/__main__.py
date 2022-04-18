9013121106cff40596f9c54a95e4c5280bf3c28a
selfishx <97510714+selfishx@users.noreply.github.com>
2022-04-18 09:34:04 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -148,9 +148,9 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
 
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="🎉 Add me to your group", url="t.me/{}?startgroup=true".format(bot.username)),  InlineKeyboardButton(text="🤖 Make Own Admin Bot", url="https://t.me/clasy_network")],
-                     [InlineKeyboardButton(text="👥 Support Group", url="https://t.me/Infinity_bots_support"), InlineKeyboardButton(text="🔔 Update Channel", url="https://t.me/invisible_avneesh")],
-                     [InlineKeyboardButton(text="🎬Chatting group", url="https://t.me/secretgroupc"), InlineKeyboardButton(text="🛠 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
+                    [[InlineKeyboardButton(text="🎉 Add me to your group", url="t.me/{}?startgroup=true".format(bot.username)),  InlineKeyboardButton(text="🤖 Make Own Admin Bot", url="https://t.me/classy_network")],
+                     [InlineKeyboardButton(text="👥 Support Group", url="https://t.me/Infinity_bots_support"), InlineKeyboardButton(text="🔔 Update Channel", url="https://t.me/INFINITY_BOTZ")],
+                     [InlineKeyboardButton(text="🎬support channel", url="https://t.me/INFINITY_BOTZ"), InlineKeyboardButton(text="🛠 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
 
     else:
         update.effective_message.reply_text("ചത്തിട്ടില്ലാ...")
