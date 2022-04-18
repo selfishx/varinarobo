002933a5c403a5363cf4fc5da529597d902930d5
selfishx <97510714+selfishx@users.noreply.github.com>
2022-04-18 08:41:26 +0530

    Update antiflood.py

--- tg_bot/modules/antiflood.py
+++ tg_bot/modules/antiflood.py
@@ -35,8 +35,8 @@ def check_flood(bot: Bot, update: Update) -> str:
 
     try:
         chat.kick_member(user.id)
-        msg.reply_text("ഫ്ലഡ് ചെയ്യുന്നോ... നിങ്ങൾക്കായി ഒരു കണ്ടം ഒരുക്കിയിട്ടുണ്ട്...  "
-                       "ഒന്ന് ഓടിയിട്ട് വരൂ...")
+        msg.reply_text("EEEEEEEEEEE GOING  "
+                       "HEHEHE...")
 
         return "<b>{}:</b>" \
                "\n#BANNED" \
